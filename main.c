a3a8a5d8db268dc21d2910c3816ea2736c173ce9
Gergő Lukács <gergolukacs@Gergos-MacBook-Air.local>
2023-06-19 23:39:16 +0200

    trying to create an image

--- main.c
+++ main.c
@@ -2,9 +2,17 @@
 #include "libft.h"
 #include "mlx.h"
 
-int	main(int argc, char *argv[])
+int	main(void)
 {
-	int count = 0;
+	void *img;
+	void *mlx;
+//	void *mlx_win;
+
+	mlx = mlx_init();
+	img = mlx_new_image(mlx, 1920, 1080);
+//	mlx_win = mlx_new_window(mlx, 1920, 1080, "hello");
+//	mlx_loop(mlx);
+/*	int count = 0;
 
 	if (argc > 2)
 		return (0);
@@ -13,5 +21,5 @@ int	main(int argc, char *argv[])
 		ft_printf("%c", argv[1][count]);
 		count++;
 	}
-	return (0);
+	return (0);*/
 }
