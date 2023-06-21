ba726c328f47db3d5a09a2f2c123e8d7ad5ba444
Gergő Lukács <gergolukacs@Gergos-MacBook-Air.local>
2023-06-21 12:11:18 +0200

    image in process

--- main.c
+++ main.c
@@ -2,6 +2,16 @@
 #include "libft.h"
 #include "mlx.h"
 
+typedef struct	s_data
+{
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+	t_data;
+}
+
 int	main(void)
 {
 	void *img;
